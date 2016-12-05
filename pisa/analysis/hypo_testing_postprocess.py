fb5cc8448c06064a1e7af4cda1adc4c18162aa19
steven-j-wren <steven.wren@icecube.wisc.edu>
2016-12-05 20:51:33 +0000

    Adding ability to remove outlying trials, and determination of p-value based on fiducial fits

--- pisa/analysis/hypo_testing_postprocess.py
+++ pisa/analysis/hypo_testing_postprocess.py
@@ -9,8 +9,16 @@ Hypothesis testing: How do two hypotheses compare for describing MC or data?
 This script/module computes significances, etc. from the logfiles recorded by
 the `hypo_testing.py` script.
 
-"""
+TODO:
 
+1) Some of the "combined" plots currently make it impossible to read the axis 
+   labels. Come up with a better way of doing this. Could involve making 
+   legends and just labelling the axes alphabetically.
+
+2) The important one - Figure out if this script generalises to the case of 
+   analysing data. My gut says it doesn't...
+
+"""
 
 from __future__ import division
 
@@ -304,7 +312,11 @@ def extract_fit(fpath, keys=None):
         Keys to extract. If None, all keys are extracted.
 
     """
-    info = from_file(fpath)
+    try:
+        info = from_file(fpath)
+    except:
+        raise RuntimeError("Cannot read from file located at %s. Something is"
+                           " potentially wrong with it. Please check."%fpath)
     if keys is None:
         return info
     if isinstance(keys, basestring):
@@ -371,75 +383,246 @@ def extract_data(data):
     return values
 
 
-def make_llr_plots(data, labels, detector, selection, outdir):
+def purge_failed_jobs(data, trial_nums, thresh=5.0):
+    '''
+    This function looks at the values of the metric and finds any it deems to 
+    be from a failed job. That is, the value of the metric fell very far 
+    outside of the rest of the values.
+
+    References:
+    ----------
+        Taken from stack overflow:
+        
+            http://stackoverflow.com/questions/22354094/pythonic-way-\
+            of-detecting-outliers-in-one-dimensional-observation-data
+
+        which references:
+
+            Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect 
+            and Handle Outliers", The ASQC Basic References in Quality Control:
+            Statistical Techniques, Edward F. Mykytka, Ph.D., Editor. 
+    '''
+    
+    for fit_key in data.keys():
+        points = np.array(data[fit_key]['metric_val']['vals'])
+        if len(points.shape) == 1:
+            points = points[:,None]
+        median = np.median(points, axis=0)
+        diff = np.sum((points - median)**2, axis=-1)
+        diff = np.sqrt(diff)
+        med_abs_deviation = np.median(diff)
+        modified_z_score = 0.6745 * diff / med_abs_deviation
+        good_trials = modified_z_score < thresh
+        if not np.all(good_trials):
+            bad_trials = np.where(good_trials==False)[0]
+            if len(bad_trials) == 1:
+                logging.warning(
+                    "Outlier detected for %s in trial %s. These will be "
+                    "removed. If you think this should not happen, please "
+                    "change the value of the threshold used for this decision "
+                    "(currently set to %.2f)."
+                    %(fit_key,trial_nums[bad_trials],thresh)
+                )
+            else:
+                logging.warning(
+                    "Outlier detected for %s in trial %s. These will be "
+                    "removed. If you think this should not happen, please "
+                    "change the value of the threshold used for this decision "
+                    "(currently set to %.2f)."
+                    %(fit_key,trial_nums[bad_trials],thresh)
+                )
+            for fitkey in data.keys():
+                for param in data[fitkey].keys():
+                    new_vals = np.delete(
+                        np.array(data[fitkey][param]['vals']),
+                        bad_trials
+                    )
+                    data[fitkey][param]['vals'] = new_vals
+                    
+
+def make_llr_plots(data, fid_data, labels, detector, selection, outdir):
     '''
     Does what you think. Takes the data and makes LLR distributions. These are 
     then saved to the requested outdir within a folder labelled 
     "LLRDistributions".
 
-    TODO - Significance calculation. This means calculating p-values and then 
-    appending the value to the plot. Probably should come up with a good way 
-    of storing this information rather than just printing it to terminal.
+    TODO:
+
+    1) p-values currently calculated assuming h0 is the truth. Should 
+       probably come up with a way of identifying the best fit and constructing 
+       the LLR distributions based on this. This is, after all, how it should 
+       be done with real data.
+
+    2) Currently the p-value is put on the LLR distributions as an annotation.
+       This is probably fine, since the significances can just be calculated 
+       from this after the fact.
+
+    3) A method of quantifying the uncertainty due to finite statistics in the 
+       pseudo-trials should be added. Possible ways are:
+        a) Fitting the distributions and then drawing X new instances of Y 
+           trials (where X and Y are large) and looking at the spread in the 
+           p-values. This would require a good fit to the distributions, which
+           is fine if they end up gaussian...
+        b) Quantifying the uncertainty on the critical value used to determine 
+           the significance. In the case of the median this can be achieved by 
+           drawing subsets of the data. Though, efforts to do this in the past
+           have given results that seemed to underestimate the true uncertainty.
     '''
     outdir = os.path.join(outdir,'LLRDistributions')
     if not os.path.exists(outdir):
         logging.info('Making output directory %s'%outdir)
         os.makedirs(outdir)
 
-    h0_fit_to_h0_fid_metrics = np.array(data['h0_fit_to_h0_fid']['metric_val'])
-    h1_fit_to_h0_fid_metrics = np.array(data['h1_fit_to_h0_fid']['metric_val'])
-    h0_fit_to_h1_fid_metrics = np.array(data['h0_fit_to_h1_fid']['metric_val'])
-    h1_fit_to_h1_fid_metrics = np.array(data['h1_fit_to_h1_fid']['metric_val'])
+    h0_fid_metric = fid_data['h0_fit_to_toy_no_asimov']['metric_val']
+    h1_fid_metric = fid_data['h1_fit_to_toy_no_asimov']['metric_val']
+
+    if h1_fid_metric > h0_fid_metric:
+        bestfit = 'h0'
+        altfit = 'h1'
+        critical_value = h0_fid_metric-h1_fid_metric
+    else:
+        bestfit = 'h0'
+        altfit = 'h1'
+        critical_value = h1_fid_metric-h0_fid_metric
+
+    h0_fit_to_h0_fid_metrics = np.array(
+        data['h0_fit_to_h0_fid']['metric_val']['vals']
+    )
+    h1_fit_to_h0_fid_metrics = np.array(
+        data['h1_fit_to_h0_fid']['metric_val']['vals']
+    )
+    h0_fit_to_h1_fid_metrics = np.array(
+        data['h0_fit_to_h1_fid']['metric_val']['vals']
+    )
+    h1_fit_to_h1_fid_metrics = np.array(
+        data['h1_fit_to_h1_fid']['metric_val']['vals']
+    )
 
     num_trials = len(h0_fit_to_h0_fid_metrics)
-    
-    LLRh0 = h0_fit_to_h0_fid_metrics - h1_fit_to_h0_fid_metrics
-    LLRh1 = h0_fit_to_h1_fid_metrics - h1_fit_to_h1_fid_metrics
+    metric_type = data['h0_fit_to_h0_fid']['metric_val']['type']
+    metric_type_pretty = make_pretty(metric_type)
 
-    minLLR = min(min(LLRh0), min(LLRh1))
-    maxLLR = max(max(LLRh0), max(LLRh1))
+    if bestfit == 'h0':
+        LLRbest = h0_fit_to_h0_fid_metrics - h1_fit_to_h0_fid_metrics
+        LLRalt = h0_fit_to_h1_fid_metrics - h1_fit_to_h1_fid_metrics
+    else:
+        LLRbest = h1_fit_to_h1_fid_metrics - h0_fit_to_h1_fid_metrics
+        LLRalt = h1_fit_to_h0_fid_metrics - h0_fit_to_h0_fid_metrics
+
+    minLLR = min(min(LLRbest), min(LLRalt))
+    maxLLR = max(max(LLRbest), max(LLRalt))
     rangeLLR = maxLLR - minLLR
     binning = np.linspace(minLLR - 0.1*rangeLLR,
                           maxLLR + 0.1*rangeLLR,
-                          num_trials/3)
+                          int(num_trials/20))
     binwidth = binning[1]-binning[0]
 
-    LLRh0hist, LLRh0binedges = np.histogram(LLRh0,bins=binning)
-    LLRh1hist, LLRh1binedges = np.histogram(LLRh1,bins=binning)
+    LLRbesthist, LLRbestbinedges = np.histogram(LLRbest,bins=binning)
+    LLRalthist, LLRaltbinedges = np.histogram(LLRalt,bins=binning)
 
-    LLRhistmax = max(max(LLRh0hist),max(LLRh1hist))
+    LLRhistmax = max(max(LLRbesthist),max(LLRalthist))
+
+    best_median = np.median(LLRbest)
+    alt_median = np.median(LLRalt)
 
     inj_name = labels['data_name']
-    h0_name = labels['h0_name']
-    h1_name = labels['h1_name']
+    best_name = labels['%s_name'%bestfit]
+    alt_name = labels['%s_name'%altfit]
 
-    plot_labels = []
-    plot_labels.append(
+    med_p_value = float(np.sum(LLRbest > alt_median))/len(LLRbest)
+    crit_p_value = float(np.sum(LLRbest > critical_value))/len(LLRbest)
+
+    med_plot_labels = []
+    med_plot_labels.append((r"Hypo %s median = %.4f"%(best_name,best_median)))
+    med_plot_labels.append((r"Hypo %s median = %.4f"%(alt_name,alt_median)))
+    med_plot_labels.append(
         (r"%s best fit - $\log\left[\mathcal{L}\left(\mathcal{H}_{%s}\right)/"
          r"\mathcal{L}\left(\mathcal{H}_{%s}\right)\right]$"
-         %(h0_name, h0_name, h1_name))
+         %(best_name, best_name, alt_name))
     )
-    plot_labels.append(
+    med_plot_labels.append(
         (r"%s best fit - $\log\left[\mathcal{L}\left(\mathcal{H}_{%s}\right)/"
          r"\mathcal{L}\left(\mathcal{H}_{%s}\right)\right]$"
-         %(h1_name, h0_name, h1_name))
+         %(alt_name, best_name, alt_name))
     )
-    plot_title = ('%s %s Event Selection LLR Distributions for true '
-                  '%s (%i trials)'
-                  %(detector,selection,inj_name,num_trials))
+
+    crit_plot_labels = []
+    crit_plot_labels.append((r"Critical value = %.4f"%(critical_value)))
+    crit_plot_labels.append(
+        (r"%s best fit - $\log\left[\mathcal{L}\left(\mathcal{H}_{%s}\right)/"
+         r"\mathcal{L}\left(\mathcal{H}_{%s}\right)\right]$"
+         %(best_name, best_name, alt_name))
+    )
+    crit_plot_labels.append(
+        (r"%s best fit - $\log\left[\mathcal{L}\left(\mathcal{H}_{%s}\right)/"
+         r"\mathcal{L}\left(\mathcal{H}_{%s}\right)\right]$"
+         %(alt_name, best_name, alt_name))
+    )
+    
+    if metric_type == 'llh':
+        plot_title = ("%s %s Event Selection "%(detector,selection)\
+                      +r"\\"+" LLR Distributions for true %s (%i trials)"
+                      %(inj_name,num_trials))
+    else:
+        plot_title = ("%s %s Event Selection "%(detector,selection)\
+                      +r"\\"+" %s \"LLR\" Distributions for "
+                      %(metric_type_pretty)\
+                      +"true %s (%i trials)"%(inj_name,num_trials))
 
     # Factor with which to make everything visible
     plot_scaling_factor = 1.55
 
-    plt.hist(LLRh0,bins=binning,color='r',histtype='step')
-    plt.hist(LLRh1,bins=binning,color='b',histtype='step')
+    plt.hist(LLRbest,bins=binning,color='r',histtype='step')
+    plt.hist(LLRalt,bins=binning,color='b',histtype='step')
     plt.xlabel(r'Log-Likelihood Ratio')
     plt.ylabel(r'Number of Trials (per %.2f)'%binwidth)
     plt.ylim(0,plot_scaling_factor*LLRhistmax)
-    plt.legend(plot_labels, loc='upper left')
+    plt.axvline(
+        best_median,
+        color='k',
+        ymax=float(max(LLRbesthist))/float(plot_scaling_factor*LLRhistmax)
+    )
+    plt.axvline(
+        alt_median,
+        color='g',
+        ymax=float(max(LLRalthist))/float(plot_scaling_factor*LLRhistmax)
+    )
+    plt.legend(med_plot_labels, loc='upper left')
     plt.title(plot_title)
-    filename = 'true_%s_%s_%s_LLRDistribution_%i_Trials.png'%(
-        inj_name, detector, selection, num_trials
+    plt.figtext(
+        0.15,
+        0.60,
+        r"p-value = $%.4f$"%(med_p_value),
+        color='k',
+        size='xx-large'
+    )
+    filename = 'true_%s_%s_%s_%s_LLRDistribution_median_%i_Trials.png'%(
+        inj_name, detector, selection, metric_type, num_trials
+    )
+    plt.savefig(os.path.join(outdir,filename))
+    plt.close()
+
+    plt.hist(LLRbest,bins=binning,color='r',histtype='step')
+    plt.hist(LLRalt,bins=binning,color='b',histtype='step')
+    plt.xlabel(r'Log-Likelihood Ratio')
+    plt.ylabel(r'Number of Trials (per %.2f)'%binwidth)
+    plt.ylim(0,plot_scaling_factor*LLRhistmax)
+    plt.axvline(
+        critical_value,
+        color='k',
+        ymax=float(max(LLRbesthist))/float(plot_scaling_factor*LLRhistmax)
+    )
+    plt.legend(crit_plot_labels, loc='upper left')
+    plt.title(plot_title)
+    plt.figtext(
+        0.15,
+        0.60,
+        r"p-value = $%.4f$"%(crit_p_value),
+        color='k',
+        size='xx-large'
+    )
+    filename = 'true_%s_%s_%s_%s_LLRDistribution_critical_%i_Trials.png'%(
+        inj_name, detector, selection, metric_type, num_trials
     )
     plt.savefig(os.path.join(outdir,filename))
     plt.close()
@@ -1131,6 +1314,13 @@ def parse_args():
         combination."""
     )
     parser.add_argument(
+        '--threshold', metavar='DIR', type=float, default=5.0,
+        help="""Sets the threshold for which to remove 'outlier' trials. 
+        Ideally this will not be needed at all, but it is there in case of
+        e.g. failed minimiser. The higher this value, the more outliers will 
+        be included. Set it to 0 if you want to include ALL trials."""
+    )
+    parser.add_argument(
         '--outdir', metavar='DIR', type=str, required=True,
         help="""Store all output plots to this directory. This will make
         further subdirectories, if needed, to organise the output plots."""
@@ -1159,6 +1349,7 @@ def main():
     ciscatter = init_args_d.pop('combined_individual_scatter')
     cscatter = init_args_d.pop('combined_scatter')
     cmatrix = init_args_d.pop('correlation_matrix')
+    threshold = init_args_d.pop('threshold')
     outdir = init_args_d.pop('outdir')
 
     if args.asimov:
@@ -1177,21 +1368,33 @@ def main():
             fluctuate_fid=True,
             fluctuate_data=False
         )
+
+        trial_nums = data_sets['toy_no_asimov']['h0_fit_to_h1_fid'].keys()
         
         fid_values = extract_fid_data(data_sets)
         values = extract_data(data_sets)
 
         for injkey in values.keys():
 
-            '''
+            if threshold != 0.0:
+                logging.info("Outlying trials will be removed with a "
+                             "threshold of %.2f"%threshold)
+                purge_failed_jobs(
+                    data = values[injkey],
+                    trial_nums = np.array(trial_nums),
+                    thresh = threshold
+                )
+            else:
+                logging.info("All trials will be included in the analysis.")
+            
             make_llr_plots(
                 data = values[injkey],
+                fid_data = fid_values[injkey],
                 labels = labels.dict,
                 detector = detector,
                 selection = selection,
                 outdir = outdir
             )
-            '''
 
             if iposteriors:
 
