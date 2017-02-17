0bf28efef5be64c514b664ec0d2bca9f92231b68
steven-j-wren <steven.wren@icecube.wisc.edu>
2017-02-17 15:56:41 +0000

    Adding output tex files that contain the best fit points and, if applicable, how they compare to the injected values

--- pisa/analysis/hypo_testing_postprocess.py
+++ pisa/analysis/hypo_testing_postprocess.py
@@ -40,29 +40,37 @@ from pisa.utils.postprocess import tex_axis_label, parse_pint_string, get_num_ro
 __all__ = ['extract_trials', 'extract_fit', 'parse_args', 'main']
 
 
-def extract_paramval(injparams, systkey, fid_label, hypo_label, paramlabel):
+def extract_paramval(injparams, systkey, fid_label=None, hypo_label=None,
+                     paramlabel=None):
     '''
     Extracts a value from a set of parameters and modifies it based on the 
     hypothesis/fiducial fit being considered. The label associated with this 
     is then modified accordingly.
     '''
     paramval = float(injparams[systkey].split(' ')[0])
-    if systkey == 'deltam31':
-        if 'no' in hypo_label:
-            if np.sign(paramval) != 1:
-                paramval = -1*float(injparams[systkey].split(' ')[0])
-                paramlabel += r' ($\times-1$)'
-        elif 'io' in hypo_label:
-            if np.sign(paramval) != -1:
-                paramval = -1*float(injparams[systkey].split(' ')[0])
-                paramlabel += r' ($\times-1$)'
+    if (fid_label == None) or (hypo_label == None) or (paramlabel == None):
+        if not ((fid_label == None) and (hypo_label == None) and
+                (paramlabel == None)):
+            raise ValueError("Either all three labels must be None or they "
+                             " must all be specified.")
+        return paramval
+    else:
+        if systkey == 'deltam31':
+            if 'no' in hypo_label:
+                if np.sign(paramval) != 1:
+                    paramval = -1*float(injparams[systkey].split(' ')[0])
+                    paramlabel += r' ($\times-1$)'
+            elif 'io' in hypo_label:
+                if np.sign(paramval) != -1:
+                    paramval = -1*float(injparams[systkey].split(' ')[0])
+                    paramlabel += r' ($\times-1$)'
 
-    if (paramval < 1e-2) and (paramval != 0.0):
-        paramlabel += ' = %.3e'%paramval
-    else:    
-        paramlabel += ' = %.3g'%paramval
+        if (np.abs(paramval) < 1e-2) and (paramval != 0.0):
+            paramlabel += ' = %.3e'%paramval
+        else:    
+            paramlabel += ' = %.3g'%paramval
 
-    return paramval, paramlabel
+        return paramval, paramlabel
     
 
 def extract_trials(logdir, fluctuate_fid, fluctuate_data=False):
@@ -810,6 +818,336 @@ def make_llr_plots(data, fid_data, labels, detector, selection, outdir):
     plt.close()
 
 
+def write_latex_preamble(texfile):
+    '''
+    Writes the latex preamble needed to be able to make tex files that look 
+    nice.
+    '''
+    texfile.write("\n")
+    texfile.write("\documentclass[a4paper,12pt]{article}\n")
+    texfile.write("\usepackage{tabu}\n")
+    texfile.write("\usepackage{booktabs}\n")
+    texfile.write("\usepackage[font=small,labelsep=space]{caption} %specifies the caption formatting for the document\n")
+    texfile.write("\usepackage[margin=2.5cm]{geometry}\n")
+    texfile.write("\setlength{\\topmargin}{1.0cm}\n")
+    texfile.write("\setlength{\\textheight}{22cm}\n")
+    texfile.write("\usepackage{fancyhdr} %allows for headers and footers\n")
+    texfile.write("\pagestyle{fancy}\n")
+    texfile.write("\\fancyhf{}\n")
+    texfile.write("\\fancyhead[R]{\leftmark}\n")
+    texfile.write("\usepackage{multirow}\n")
+    texfile.write("\n")
+    texfile.write("\\begin{document}\n")
+    texfile.write("\n")
+
+
+def setup_latex_table(texfile, tabletype, injected=False):
+    '''
+    Sets up the beginning of the table for the tex output files. Currently will
+    make tables for the output fiducial fit params and the chi2 values only.
+    '''
+    texfile.write("\\renewcommand{\\arraystretch}{1.6}\n")
+    texfile.write("\n")
+    texfile.write("\\begin{table}[t!]\n")
+    texfile.write("  \\begin{center}\n")
+    if tabletype == 'fiducial_fit_params':
+        if injected:
+            texfile.write("    \\begin{tabu} to 1.0\\textwidth {| X[2.0,c] | X[1,c] | X[1,c] | X[1,c] | X[1,c] | X[1,c] | X[1,c] | X[1,c] |}\n")
+            texfile.write("    \hline\n")
+            texfile.write("    \multirow{2}{*}{\\textbf{Parameter}} & \multirow{2}{*}{\\textbf{Inj}} & \multicolumn{3}{c|}{h0} & \multicolumn{3}{c|}{h1} \\\\ \cline{3-8}")
+            texfile.write("    & & Prior & Fit & \(\Delta\) & Prior & Fit & \(\Delta\) \\\\ \hline\n")
+        else:
+            texfile.write("    \\begin{tabu} to 1.0\\textwidth {| X[c] | X[c] | X[c] |}\n")
+            texfile.write("    \hline\n")
+            texfile.write("    Parameter & h0 & h1 \\\\ \hline\n")
+    elif tabletype == 'fiducial_fit_metrics':
+        texfile.write("    \\begin{tabu} to 1.0\\textwidth {| X[c] | X[c] | X[c] |}\n")
+        texfile.write("    \hline\n")
+        texfile.write("    h0 & h1 & $\Delta$ \\\\ \hline\n")
+    else:
+        raise ValueError("This function is only for making fit metric or fit "
+                         "param tables in LaTeX.")
+
+
+def end_latex_file(texfile, tabletype, detector, selection, h0, h1, truth=None):
+    '''
+    Ends the table and the whole document for the tex output files.
+    '''
+    if tabletype == 'fiducial_fit_params':
+        texfile.write("    \end{tabu}\n")
+        texfile.write("  \end{center}\n")
+        texfile.write("  \\vspace{-10pt}\n")
+        if truth is not None:
+            texfile.write("  \caption{shows the fiducial fit parameters obtained with the %s %s sample for h0 of %s and h1 of %s. The truth is %s.}\n"%(detector,selection,h0,h1,truth))
+        else:
+            texfile.write("  \caption{shows the fiducial fit parameters obtained with the %s %s sample for h0 of %s and h1 of %s.}\n"%(detector,selection,h0,h1))
+        texfile.write("  \label{tab:%s%s%stable}\n"%(detector,selection,tabletype))
+        texfile.write("\end{table}\n")
+        texfile.write("\n")
+        texfile.write("\end{document}\n")
+    elif tabletype == 'fiducial_fit_metrics':
+        texfile.write("    \end{tabu}\n")
+        texfile.write("  \end{center}\n")
+        texfile.write("  \\vspace{-10pt}\n")
+        if truth is not None:
+            texfile.write("  \caption{shows the fiducial fit metrics obtained with the %s %s sample for h0 of %s and h1 of %s. The truth is %s.}\n"%(detector,selection,h0,h1,truth))
+        else:
+            texfile.write("  \caption{shows the fiducial fit metrics obtained with the %s %s sample for h0 of %s and h1 of %s.}\n"%(detector,selection,h0,h1))
+        texfile.write("  \label{tab:%s%s%stable}\n"%(detector,selection,tabletype))
+        texfile.write("\end{table}\n")
+        texfile.write("\n")
+        texfile.write("\end{document}\n")
+
+
+def format_table_line(val, dataval, stddev=None, maximum=None, last=False):
+    '''
+    Formatting the numbers to look nice is awkard so do it in its' own function
+    '''
+    line = ""
+    if stddev is not None:
+        if (np.abs(stddev) < 1e-2) and (stddev != 0.0):
+            line += r'$%.3e\pm%.3e$ &'%(maximum,stddev)
+        else:
+            line += r'$%.3g\pm%.3g$ &'%(maximum,stddev)
+    else:
+        if maximum is not None:
+            raise ValueError("Both stddev and maximum should be None or "
+                             "specified")
+        else:
+            line += "-- &"
+    if (np.abs(val) < 1e-2) and (val != 0.0):
+        line += "%.3e"%val
+    else:
+        line += "%.3g"%val
+    if dataval is not None:
+        line += " &"
+        if isinstance(dataval, basestring):
+            line += "%s"%dataval
+        else:
+            delta = val - dataval
+            if (np.abs(delta) < 1e-2) and (delta != 0.0):
+                line += "%.3e"%delta
+            else:
+                line += "%.3g"%delta
+    if not last:
+            line += " &"
+    return line
+
+
+def make_fiducial_plots(data, fid_data, labels, all_params, detector,
+                        selection, outdir):
+    '''
+    This function will make tex files which can be then be compiled in to 
+    tables showing the two fiducial fits and, if applicable, how they compare 
+    to what was injected.
+    '''
+    outdir = os.path.join(outdir,'FiducialFits')
+    if not os.path.exists(outdir):
+        logging.info('Making output directory %s'%outdir)
+        os.makedirs(outdir)
+
+    # Make output file to write to
+    paramfilename = ("true_%s_%s_%s_fiducial_fit_params.tex"
+                     %(labels['data_name'],
+                       detector,
+                       selection))
+    paramfile = os.path.join(outdir,paramfilename)
+    pf = open(paramfile, 'w')
+    write_latex_preamble(texfile = pf)
+
+    h0_params = fid_data[
+        ('h0_fit_to_toy_%s_asimov'%labels['data_name'])
+    ]['params']
+    h1_params = fid_data[
+        ('h1_fit_to_toy_%s_asimov'%labels['data_name'])
+    ]['params']
+
+    if 'data_params' in all_params.keys():
+        data_params = {}
+        for pkey in all_params['data_params'].keys():
+            data_params[pkey] = all_params['data_params'][pkey]['value']
+        injected = True
+    else:
+        data_params = None
+        injected = False
+
+    setup_latex_table(
+        texfile = pf,
+        tabletype = 'fiducial_fit_params',
+        injected=injected
+    )
+
+    for param in h0_params.keys():
+        # Get the units for this parameter
+        val, param_units = parse_pint_string(
+            pint_string=fid_data[
+                'h0_fit_to_toy_%s_asimov'%labels['data_name']
+            ]['params'][param]
+        )
+        # Get priors if they exists
+        if 'gaussian' in all_params['h0_params'][param]['prior']:
+            h0stddev, h0maximum = extract_gaussian(
+                prior_string = all_params['h0_params'][param]['prior'],
+                units = param_units
+            )
+        else:
+            h0stddev = None
+        if 'gaussian' in all_params['h1_params'][param]['prior']:
+            h1stddev, h1maximum = extract_gaussian(
+                prior_string = all_params['h1_params'][param]['prior'],
+                units = param_units
+            )
+        else:
+            h1stddev = None
+        # Include injected parameter, fitted parameters and differences with
+        # appropriate formatting.
+        if data_params is not None:
+            tableline = "      "
+            tableline += "%s "%tex_axis_label(param)
+            if param == 'deltam31':
+                tableline += r" / $10^{-3}$ "
+            if param_units != 'dimensionless':
+                tableline += "(%s) &"%tex_axis_label(param_units)
+            else:
+                tableline += "&"
+            if param in data_params.keys():
+                dataval = extract_paramval(
+                    injparams = data_params,
+                    systkey = param
+                )
+                if param == 'deltam31':
+                    dataval *= 1000.0
+                if (np.abs(dataval) < 1e-2) and (dataval != 0.0):
+                    tableline += "%.3e &"%dataval
+                else:
+                    tableline += "%.3g &"%dataval
+            # If no injected parameter, show this and the deltas with a line
+            else:
+                dataval = '--'
+                tableline += "%s &"%dataval
+            h0val = extract_paramval(
+                injparams = h0_params,
+                systkey = param
+            )
+            if param == 'deltam31':
+                h0val *= 1000.0
+            if h0stddev is not None:
+                tableline += format_table_line(val=h0val, dataval=dataval,
+                                               stddev=h0stddev,
+                                               maximum=h0maximum)
+            else:
+                tableline += format_table_line(val=h0val, dataval=dataval)
+            h1val = extract_paramval(
+                injparams = h1_params,
+                systkey = param
+            )
+            if param == 'deltam31':
+                h1val *= 1000.0
+            if h1stddev is not None:
+                tableline += format_table_line(val=h1val, dataval=dataval,
+                                               stddev=h1stddev,
+                                               maximum=h1maximum, last=True)
+            else:
+                tableline += format_table_line(val=h1val, dataval=dataval,
+                                               last=True)
+            tableline += " \\\\ \hline\n"
+            pf.write(tableline)
+        # If no injected parameters it's much simpler
+        else:
+            h0val = extract_paramval(
+                injparams = h0_params,
+                systkey = param
+            )
+            h1val = extract_paramval(
+                injparams = h1_params,
+                systkey = param
+            )
+            if (np.abs(h0val) < 1e-2) and (h0val != 0.0):
+                pf.write("    %s & %.3e & %.3e\n"%(h0val, h1val))
+            else:
+                pf.write("    %s & %.3g & %.3g\n"%(h0val, h1val))
+
+    end_latex_file(
+        texfile=pf,
+        tabletype='fiducial_fit_params',
+        detector=detector,
+        selection=selection,
+        h0 = tex_axis_label(labels['h0_name']),
+        h1 = tex_axis_label(labels['h1_name']),
+        truth = tex_axis_label(labels['data_name'])
+    )
+
+    # Make output file to write to
+    metricfilename = ("true_%s_%s_%s_fiducial_fit_metrics.tex"
+                     %(labels['data_name'],
+                       detector,
+                       selection))
+    metricfile = os.path.join(outdir,metricfilename)
+    mf = open(metricfile, 'w')
+    write_latex_preamble(texfile = mf)
+
+    setup_latex_table(
+        texfile = mf,
+        tabletype = 'fiducial_fit_metrics'
+    )
+
+    h0_fid_metric = fid_data[
+        'h0_fit_to_toy_%s_asimov'%labels['data_name']
+    ][
+        'metric_val'
+    ]
+    h1_fid_metric = fid_data[
+        'h1_fit_to_toy_%s_asimov'%labels['data_name']
+    ][
+        'metric_val'
+    ]
+
+    metric_type = data['h0_fit_to_h0_fid']['metric_val']['type']
+    metric_type_pretty = tex_axis_label(metric_type)
+    # In the case of likelihood, the maximum metric is the better fit.
+    # With chi2 metrics the opposite is true, and so we must multiply
+    # everything by -1 in order to apply the same treatment.
+    if 'chi2' not in metric_type:
+        logging.info("Converting likelihood metric to chi2 equivalent.")
+        h0_fid_metric *= -1
+        h1_fid_metric *= -1
+
+    # If truth is known, report the fits the correct way round
+    if labels['data_name'] is not None:
+        if labels['data_name'] in labels['h0_name']:
+            delta = h1_fid_metric-h0_fid_metric
+        elif labels['data_name'] in labels['h1_name']:
+            delta = h0_fid_metric-h1_fid_metric
+        else:
+            logging.warning("Truth is known but could not be identified in "
+                            "either of the hypotheses. The difference between"
+                            " the best fit metrics will just be reported as "
+                            "positive and so will not necessarily reflect if "
+                            "the truth was recovered.")
+            if h1_fid_metric > h0_fid_metric:
+                delta = h0_fid_metric-h1_fid_metric
+            else:
+                delta = h1_fid_metric-h0_fid_metric
+    # Else just report it as delta between best fits
+    else:
+        if h1_fid_metric > h0_fid_metric:
+            delta = h0_fid_metric-h1_fid_metric
+        else:
+            delta = h1_fid_metric-h0_fid_metric
+    # Write this in the file
+    mf.write("    %.3g & %.3g & %.3g \\\\ \hline\n"%(h0_fid_metric, h1_fid_metric, delta))
+    # Then end the file
+    end_latex_file(
+        texfile=mf,
+        tabletype='fiducial_fit_metrics',
+        detector=detector,
+        selection=selection,
+        h0 = tex_axis_label(labels['h0_name']),
+        h1 = tex_axis_label(labels['h1_name']),
+        truth = tex_axis_label(labels['data_name'])
+    )
+    
+
 def plot_individual_posterior(data, data_params, h0_params, h1_params,
                               all_params, labels, h0label, h1label, systkey,
                               fhkey, subplotnum=None):
@@ -898,7 +1236,7 @@ def plot_individual_posterior(data, data_params, h0_params, h1_params,
                     units = units
                 )
                 currentxlim = plt.xlim()
-                if (stddev < 1e-2) and (stddev != 0.0):
+                if (np.abs(stddev) < 1e-2) and (stddev != 0.0):
                     priorlabel = (r'Gaussian Prior '
                                   '($%.3e\pm%.3e$)'%(maximum,stddev))
                 else:
@@ -1602,10 +1940,22 @@ def main():
             else:
                 logging.info("All trials will be included in the analysis.")
 
+            '''
             make_llr_plots(
                 data = values[injkey],
                 fid_data = fid_values[injkey],
                 labels = labels.dict,
+                detector = detector,
+                selection = selection,
+                outdir = outdir
+            )
+            '''
+
+            make_fiducial_plots(
+                data = values[injkey],
+                fid_data = fid_values[injkey],
+                labels = labels.dict,
+                all_params = all_params,
                 detector = detector,
                 selection = selection,
                 outdir = outdir
