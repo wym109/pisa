91d2558d1c2329e3f38da22cb32904857343c41b
Philipp Eller <peller@phys.ethz.ch>
2017-11-17 10:43:45 -0500

    missing semicolon and returnstatement

--- pisa/stages/mc/GPUWeight.py
+++ pisa/stages/mc/GPUWeight.py
@@ -135,8 +135,9 @@ class GPUWeight(object):
         }
         if (kFlav == 1) {
             //A_shape = 1.*abs(uphor)*LogLogParam(true_energy, z1max_mu, z2max_mu, x1z, x2z, true, numu_cutoff);
-            return 1.
+            return 1.;
         }
+        return 1.;
     }
 
     __device__ fType modRatioNuBar(const int kNuBar, const int kFlav, fType true_e, fType true_cz, fType nu_nubar, fType nubar_sys) {
