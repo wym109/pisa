bugfix: clip to min bounds if within precision (#428)
* bugfix: clip to min bounds if within precision

* Minimizer start values depend on clipping. Only warn about values coinciding with bounds after that.

pisa/analysis/analysis.py
@@ -16,9 +16,10 @@ import time
 import numpy as np
 import scipy.optimize as optimize
 
-from pisa import FTYPE, ureg
+from pisa import EPSILON, FTYPE, ureg
 from pisa.core.map import Map, MapSet
 from pisa.core.param import ParamSet
+from pisa.utils.comparisons import recursiveEquality
 from pisa.utils.log import logging
 from pisa.utils.fileio import to_file
 from pisa.utils.stats import METRICS_TO_MAXIMIZE
@@ -523,30 +524,36 @@ class Analysis(object):
         else:
             bounds = [(0, 1)]*len(x0)
 
+        clipped_x0 = []
         for param, x0_val, bds in zip(hypo_maker.params.free, x0, bounds):
-            if x0_val < bds[0]:
+            if x0_val < bds[0] - EPSILON:
                 raise ValueError(
-                    'Param %s, initial value %e exceeds the lower bound.'
-                    % (param.name, param.value)
+                    'Param %s, initial scaled value %.17e is below lower bound'
+                    ' %.17e.' % (param.name, x0_val, bds[0])
                 )
-            if x0_val > bds[1]:
+            if x0_val > bds[1] + EPSILON:
                 raise ValueError(
-                    'Param %s, initial value %e exceeds the upper bound.'
-                    % (param.name, param.value)
+                    'Param %s, initial scaled value %.17e exceeds upper bound'
+                    ' %.17e.' % (param.name, x0_val, bds[1])
                 )
 
-            if np.isclose(x0_val, bds[0], atol=np.finfo(FTYPE).eps, rtol=0):
+            clipped_x0_val = np.clip(x0_val, a_min=bds[0], a_max=bds[1])
+            clipped_x0.append(clipped_x0_val)
+
+            if recursiveEquality(clipped_x0_val, bds[0]):
                 logging.warn(
-                    'Param %s, initial value value %e is at the lower bound;'
+                    'Param %s, initial scaled value %e is at the lower bound;'
                     ' minimization may fail as a result.',
-                    param.name, param.value
+                    param.name, clipped_x0_val
                 )
-            if np.isclose(x0_val, bds[1], atol=np.finfo(FTYPE).eps, rtol=0):
+            if recursiveEquality(clipped_x0_val, bds[1]):
                 logging.warn(
-                    'Param %s, initial value value %e is at the upper bound;'
-                    ' minimization may fail as a rsult.',
-                    param.name, param.value
+                    'Param %s, initial scaled value %e is at the upper bound;'
+                    ' minimization may fail as a result.',
+                    param.name, clipped_x0_val
                 )
+
+        x0 = tuple(clipped_x0)
 
         logging.debug('Running the %s minimizer...', minimizer_method)
 