4186458e3104878af64f462147092a6f760a9a5c
Philipp Eller <pde3@psu.edu>
2018-01-18 15:50:15 +0100

    Pisa pi mceq barr sys (#451)
    * initial mceq stuff
    * in the middle of work
    * something starting to work
    * first working version of mceq barr sys
    * updating docstring

---
+++ pisa/scripts/create_barr_sys_tables_mceq.py
@@ -0,0 +1,233 @@
+# Calculates the differentials for Jacobian matrix wrt. particle
+# production uncertainty.
+#
+# Author: Anatoli Fedynitch
+# August 2017
+#
+
+
+import os, sys, gzip, bz2
+import numpy as np
+import cPickle as pickle
+
+from scipy.interpolate import RectBivariateSpline
+
+sys.path.append('../../MCEq')
+#sys.path.append('calculations')
+
+#import solver related modules
+from MCEq.core import MCEqRun
+from MCEq.misc import normalize_hadronic_model_name
+from mceq_config import config, mceq_config_without
+
+#import primary model choices
+import CRFluxModels as pm
+
+# Global Barr parameter table
+# format (x_min, x_max, E_min, E_max) | x is x_lab= E_pi/E, E projectile-air interaction energy
+barr = {
+    'a': [(0.0, 0.5, 0.00, 8.0)],
+    'b1': [(0.5, 1.0, 0.00, 8.0)],
+    'b2': [(0.6, 1.0, 8.00, 15.0)],
+    'c': [(0.2, 0.6, 8.00, 15.0)],
+    'd1': [(0.0, 0.2, 8.00, 15.0)],
+    'd2': [(0.0, 0.1, 15.0, 30.0)],
+    'd3': [(0.1, 0.2, 15.0, 30.0)],
+    'e': [(0.2, 0.6, 15.0, 30.0)],
+    'f': [(0.6, 1.0, 15.0, 30.0)],
+    'g': [(0.0, 0.1, 30.0, 1e11)],
+    'h1': [(0.1, 1.0, 30.0, 500.)],
+    'h2': [(0.1, 1.0, 500.0, 1e11)],
+    'i': [(0.1, 1.0, 500.0, 1e11)],
+    'w1': [(0.0, 1.0, 0.00, 8.0)],
+    'w2': [(0.0, 1.0, 8.00, 15.0)],
+    'w3': [(0.0, 0.1, 15.0, 30.0)],
+    'w4': [(0.1, 0.2, 15.0, 30.0)],
+    'w5': [(0.0, 0.1, 30.0, 500.)],
+    'w6': [(0.0, 0.1, 500., 1e11)],
+    'x': [(0.2, 1.0, 15.0, 30.0)],
+    'y1': [(0.1, 1.0, 30.0, 500.)],
+    'y2': [(0.1, 1.0, 500., 1e11)],
+    'z': [(0.1, 1.0, 500., 1e11)],
+    'ch_a': [(0.0, 0.1, 0., 1e11)],
+    'ch_b': [(0.1, 1.0, 0., 1e11)],
+    'ch_e': [(0.1, 1.0, 800., 1e11)],
+}
+
+
+def barr_unc(xmat, egrid, pname, value):
+    """Implementation of hadronic uncertainties as in Barr et al. PRD 74 094009 (2006)
+
+    The names of parameters are explained in Fig. 2 and Fig. 3 in the paper."""
+
+    # Energy dependence
+    u = lambda E, val, ethr, maxerr, expected_err: val*min(
+        maxerr/expected_err,
+        0.122/expected_err*np.log10(E / ethr)) if E > ethr else 0.
+
+    modmat = np.ones_like(xmat)
+    modmat[np.tril_indices(xmat.shape[0], -1)] = 0.
+
+    for minx, maxx, mine, maxe in barr[pname]:
+        eidcs = np.where((mine < egrid) & (egrid <= maxe))[0]
+        for eidx in eidcs:
+            xsel = np.where((xmat[:eidx + 1, eidx] >= minx) &
+                            (xmat[:eidx + 1, eidx] <= maxx))[0]
+            if not np.any(xsel):
+                continue
+            if pname in ['i', 'z']:
+                modmat[xsel, eidx] += u(egrid[eidx], value, 500., 0.5, 0.122)
+            elif pname in ['ch_e']:
+                modmat[xsel, eidx] += u(egrid[eidx], value, 800., 0.3, 0.25)
+            else:
+                modmat[xsel, eidx] += value
+
+    return modmat
+
+
+def compute_abs_derivatives(mceq_run, pid, barr_param, zenith_list):
+    mceq_run.unset_mod_pprod(dont_fill=False)
+
+    barr_pars = [p for p in barr if p.startswith(barr_param) and 'ch' not in p]
+    print 'Parameters corresponding to selection', barr_pars
+    dim_res = len(zenith_list), etr.shape[0]
+    gs = mceq_run.get_solution
+
+    numu, anumu, nue, anue = (np.zeros(dim_res), np.zeros(dim_res),
+                              np.zeros(dim_res), np.zeros(dim_res))
+
+    for iz, zen_deg in enumerate(zenith_list):
+        mceq_run.set_theta_deg(zen_deg)
+        mceq_run.solve()
+        numu[iz] = gs('total_numu', 0)[tr]
+        anumu[iz] = gs('total_antinumu', 0)[tr]
+        nue[iz] = gs('total_nue', 0)[tr]
+        anue[iz] = gs('total_antinue', 0)[tr]
+
+    mceq_run.unset_mod_pprod(dont_fill=True)
+    for p in barr_pars:
+        mceq_run.set_mod_pprod(2212, pid, barr_unc, (p, delta))
+#     mceq_run.y.print_mod_pprod()
+    mceq_run._init_default_matrices(skip_D_matrix=True)
+
+    numu_up, anumu_up, nue_up, anue_up = (np.zeros(dim_res), np.zeros(dim_res),
+                                          np.zeros(dim_res), np.zeros(dim_res))
+    for iz, zen_deg in enumerate(zenith_list):
+        mceq_run.set_theta_deg(zen_deg)
+        mceq_run.solve()
+        numu_up[iz] = gs('total_numu', 0)[tr]
+        anumu_up[iz] = gs('total_antinumu', 0)[tr]
+        nue_up[iz] = gs('total_nue', 0)[tr]
+        anue_up[iz] = gs('total_antinue', 0)[tr]
+
+    mceq_run.unset_mod_pprod(dont_fill=True)
+    for p in barr_pars:
+        mceq_run.set_mod_pprod(2212, pid, barr_unc, (p, -delta))
+
+#     mceq_run.y.print_mod_pprod()
+    mceq_run._init_default_matrices(skip_D_matrix=True)
+
+    numu_down, anumu_down, nue_down, anue_down = (np.zeros(dim_res),
+                                                  np.zeros(dim_res),
+                                                  np.zeros(dim_res),
+                                                  np.zeros(dim_res))
+    for iz, zen_deg in enumerate(zenith_list):
+        mceq_run.set_theta_deg(zen_deg)
+        mceq_run.solve()
+        numu_down[iz] = gs('total_numu', 0)[tr]
+        anumu_down[iz] = gs('total_antinumu', 0)[tr]
+        nue_down[iz] = gs('total_nue', 0)[tr]
+        anue_down[iz] = gs('total_antinue', 0)[tr]
+
+    fd_derivative = lambda up, down: (up - down) / (2. * delta)
+
+    dnumu = fd_derivative(numu_up, numu_down)
+    danumu = fd_derivative(anumu_up, anumu_down)
+    dnue = fd_derivative(nue_up, nue_down)
+    danue = fd_derivative(anue_up, anue_down)
+    return [
+        RectBivariateSpline(cos_theta, np.log(etr), dist)
+        for dist in [numu, dnumu, anumu, danumu, nue, dnue, anue, danue]
+    ]
+
+if __name__ == '__main__':
+    iamod = normalize_hadronic_model_name(sys.argv[1]) #the interaction model
+    iatag = sys.argv[2] # Tag name for the interaction model
+    
+    CRModel = pm.GaisserHonda
+
+    print 'Running with', iamod
+    idjob = 0  # int(os.path.expandvars('$SGE_TASK_ID')) - 1
+
+    mceq_run = MCEqRun(
+        #provide the string of the interaction model
+        interaction_model=iamod,
+        #primary cosmic ray flux model
+        #support a tuple (primary model class (not instance!), arguments)
+        primary_model=(CRModel, None),
+        # Zenith angle in degrees. 0=vertical, 90=horizontal
+        theta_deg=0.,
+        #GPU device id
+        **config)
+
+    # Some global settings. One can play around with them, but there
+    # is currently no good reason why
+
+    # Primary proton projectile (neutron is included automatically
+    # vie isosping symmetries)
+    p = 2212
+    # The parameter delta for finite differences computation
+    delta = 0.001
+    # Energy grid will be truncated below this value (saves some
+    # memory and interpolation speed, but not really needed, I think)
+    E_tr = 1e5
+
+    # Set density/atmosphere model:
+    # For a yearly average of the global atmosphere, the US Std.
+    # atmosphere is sufficiently accurate. It would be wrong to
+    # choose here anything related to South Pole, since stuff
+    # comes from/from below horizon.
+    mceq_run.set_density_model(('CORSIKA', ('BK_USStd', None)))
+
+    # Define equidistant grid in cos(theta) for 2D interpolation
+    # (Can be increased to 20 after debugging is done)
+    # The flux without propagation effects and atmospheric variations
+    # is up/down symmetric.
+    cos_theta = np.linspace(0, 1, 21)
+    angles = np.arccos(cos_theta) / np.pi * 180.
+
+    # Some technical shortcuts
+    solution = {}
+    gs = mceq_run.get_solution
+    pidx = mceq_run.pdg2pref[2212].lidx()
+    nidx = mceq_run.pdg2pref[2112].lidx()
+    mag = 0.
+    tr = np.where(mceq_run.e_grid < 1e5)
+    etr = mceq_run.e_grid[tr]
+
+    # Barr variables related to pions
+    barr_pivars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+    for bp in barr_pivars:
+        # if bp != 'h':
+        #     continue
+        solution[bp + '+'] = compute_abs_derivatives(mceq_run, 211, bp, angles)
+        solution[bp + '-'] = compute_abs_derivatives(mceq_run, -211, bp, angles)
+
+    # Barr variables related to kaons
+    barr_kvars = ['w', 'y', 'z']
+
+    for bp in barr_kvars:
+        # if bp != 'y':
+        #     continue
+        solution[bp + '+'] = compute_abs_derivatives(mceq_run, 321, bp, angles)
+        solution[bp + '-'] = compute_abs_derivatives(mceq_run, -321, bp, angles)
+
+    pickle.dump(
+        solution,
+        bz2.BZ2File(
+            os.path.join(
+                os.getcwd(),
+                'SA_superfast_jacobians_20170820_1e5_GH_{0}.ppd.bz2'.format(iatag)),
+            'wb'),
+        protocol=-1)
