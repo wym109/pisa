1180c654dab231bbcd7721fd78a8af2c6a31e149
Jan Weldert <32642322+JanWeldert@users.noreply.github.com>
2023-04-25 10:28:32 -0400

    Make genie sys stage more generic  (#732)
    * increase numba requirement
    
    * Fixes for test notebook
    
    * Update detectors class and add test notebook
    
    * Make it possible to weight events
    
    * adjust hypersurfaces for weighted events
    
    * Update hist.py
    
    * update multi det notebook
    
    * Add update_param_values function for Detector class
    
    * add test script for Detectors class
    
    * Add Detectors test and (somewhat) fix DistributionMaker test
    
    * only change bin uncertainties if they exist
    
    * make genie sys stage more generic
    
    * more flexibility when splitting interactions
    
    * Handle __builtins__ both as dict and as module
    
    * cythonize
    
    * move cython import
    
    * remove cython (seems like I do not know what I am doing)
    
    ---------
    
    Co-authored-by: Jan Weldert <jweldert@cobalt01.icecube.wisc.edu>
    Co-authored-by: Jan Weldert <jweldert@cobalt04.icecube.wisc.edu>
    Co-authored-by: Jan Weldert <jweldert@cobalt03.icecube.wisc.edu>
    Co-authored-by: Jan Weldert <jweldert@cobalt06.icecube.wisc.edu>
    Co-authored-by: Jan Weldert <jweldert@cobalt07.icecube.wisc.edu>

--- pisa/stages/xsec/genie_sys.py
+++ pisa/stages/xsec/genie_sys.py
@@ -4,10 +4,11 @@ Stage to apply pre-calculated Genie uncertainties
 
 from __future__ import absolute_import, print_function, division
 
-__all__ = ["genie_sys", "SIGNATURE", "apply_genie_sys"]
+__all__ = ["genie_sys", "apply_genie_sys"] #, "SIGNATURE"
 
+import re
 import numpy as np
-from numba import guvectorize
+#from numba import guvectorize
 
 from pisa import FTYPE, TARGET
 from pisa.core.stage import Stage
@@ -23,32 +24,32 @@ class genie_sys(Stage): # pylint: disable=invalid-name
     ----------
     params
         Must contain ::
-
-            Genie_Ma_QE : quantity (dimensionless)
-            Genie_Ma_RES : quantity (dimensionless)
+        
+        parameters specified in interactions (dimensionless)
 
     Notes
     -----
-    Requires the events have the following keys ::
+    Requires the events have the following keys for each included interaction ::
 
-        linear_fit_maccqe
-            Genie CC quasi elastic linear coefficient
-        quad_fit_maccqe
-            Genie CC quasi elastic quadratic coefficient
-        linear_fit_maccres
-            Genie CC resonance linear coefficient
-        quad_fit_maccres
-            Genie CC resonance quadratic coefficient
+        linear_fit_{name}
+            Genie linear coefficient for interaction {name}
+        quad_fit_{name}
+            Genie quadratic coefficient for interaction {name}
 
     """
     def __init__(
         self,
+        interactions="Genie_Ma_QE, Genie_Ma_RES",
+        names="maccqe, maccres",
         **std_kwargs,
     ):
-        expected_params = (
-            'Genie_Ma_QE',
-            'Genie_Ma_RES',
-        )
+        interactions = re.split(r'\W+', interactions)
+        names = re.split(r'\W+', names)
+        assert len(interactions) == len(names), 'Specify a name for each interaction'
+        
+        expected_params = tuple(interactions)
+        self.interactions = interactions
+        self.names = names
 
         # init base class
         super().__init__(
@@ -59,29 +60,28 @@ class genie_sys(Stage): # pylint: disable=invalid-name
 
     def setup_function(self):
         '''
-        Check the range of the axial masses parameter
-        in the analysis. Send a warning if these are beyond +- 2sigma
+        Check the range of the parameter in the analysis. Send a warning if these are beyond +- 2sigma
         '''
-        if self.params['Genie_Ma_QE'].range[0]<-2. or self.params['Genie_Ma_QE'].range[1]>2.:
-            logging.warn('Genie_Ma_QE parameter bounds have been set larger than the range used to produce interpolation points ([-2.,2]). This will void the warranty...')
-        if self.params['Genie_Ma_RES'].range[0]<-2. or self.params['Genie_Ma_RES'].range[1]>2.:
-            logging.warn('Genie_Ma_RES parameter bounds have been set larger than the range used to produce interpolation points ([-2.,2]). This will void the warranty...')
-
-
+        for I in self.interactions:
+            if self.params[I].range[0]<-2. or self.params[I].range[1]>2.:
+                logging.warn(I+' parameter bounds have been set larger than the range used to produce interpolation points ([-2.,2]). This will void the warranty...')
 
 
     def apply_function(self):
-        genie_ma_qe = self.params.Genie_Ma_QE.m_as('dimensionless')
-        genie_ma_res = self.params.Genie_Ma_RES.m_as('dimensionless')
+        genie_params = []
+        for I in self.interactions:
+            exec("genie_params.append(self.params.%s.m_as('dimensionless'))"%(I))
 
         for container in self.data:
+            linear_fits, quad_fits = [], []
+            for i in range(len(self.interactions)):
+                linear_fits.append(container['linear_fit_'+self.names[i]])
+                quad_fits.append(container['quad_fit_'+self.names[i]])
+
             apply_genie_sys(
-                genie_ma_qe,
-                container['linear_fit_maccqe'],
-                container['quad_fit_maccqe'],
-                genie_ma_res,
-                container['linear_fit_maccres'],
-                container['quad_fit_maccres'],
+                genie_params,
+                linear_fits,
+                quad_fits,
                 out=container['weights'],
             )
 
@@ -94,21 +94,18 @@ class genie_sys(Stage): # pylint: disable=invalid-name
 
 
 
-if FTYPE == np.float64:
-    SIGNATURE = '(f8, f8, f8, f8, f8, f8, f8[:])'
-else:
-    SIGNATURE = '(f4, f4, f4, f4, f4, f4, f4[:])'
-@guvectorize([SIGNATURE], '(),(),(),(),(),()->()', target=TARGET)
+#if FTYPE == np.float64:
+#    SIGNATURE = '(f8, f8, f8, f8[:])'
+#else:
+#    SIGNATURE = '(f4, f4, f4, f4[:])'
+#@guvectorize([SIGNATURE], '(),(),()->()', target=TARGET)
 def apply_genie_sys(
-    genie_ma_qe,
-    linear_fit_maccqe,
-    quad_fit_maccqe,
-    genie_ma_res,
-    linear_fit_maccres,
-    quad_fit_maccres,
+    genie_params,
+    linear_fits,
+    quad_fits,
     out,
 ):
-    out[0] *= max(0, (
-        (1. + (linear_fit_maccqe + quad_fit_maccqe * genie_ma_qe) * genie_ma_qe)
-        * (1. + (linear_fit_maccres + quad_fit_maccres * genie_ma_res) * genie_ma_res)
-    ))
+    factor = 1
+    for i in range(len(genie_params)):
+        factor *= 1. + (linear_fits[i] + quad_fits[i] * genie_params[i]) * genie_params[i]
+    out *= np.maximum(0, factor)
